aa034ac52fd1b5e393e14c80552bfac50881a5a2
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-12-15 20:49:36 -0300

    add regex-boilerplate file

---
+++ regex-boilerplate.py
@@ -0,0 +1,21 @@
+import os
+import re
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+os.chdir(
+    os.path.join(os.getenv('REPO_PATH'), 'folder')
+) 
+
+with open(os.path.join('.', 'data.txt'), 'r', encoding='utf8') as f:
+    data = f.read()
+    
+    # 1- Statement
+    match = re.findall(
+        r"""
+            # some regex pattern here
+        """, 
+        data, flags=re.VERBOSE
+    )
+    print(match)
